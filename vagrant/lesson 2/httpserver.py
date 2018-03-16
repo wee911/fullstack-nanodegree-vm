f1ab7b16b0d80347ff14107f52650c1fde9134aa
qzy <weelgod@gmail.com>
2018-03-16 20:15:13 +0800

    lesson2 finish

--- vagrant/lesson 2/httpserver.py
+++ vagrant/lesson 2/httpserver.py
@@ -32,6 +32,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(bytes(message, 'utf8'))
             print(message)
             return
+
         if self.path.endswith("/hola"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -41,6 +42,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.wfile.write(bytes(message, 'utf8'))
             print(message)
             return
+
         if self.path.endswith("/restaurants"):
             restaurants = session.query(Restaurant).all()
             output = ""
@@ -54,13 +56,70 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 print(restaurant.name)
                 output += restaurant.name
                 output += "</br>"
-                output += "<a href='#'>Edit</a>"
+                output += "<a href='/restaurants/%s/edit'>edit</a>" % restaurant.id
                 output += "</br>"
-                output += "<a href='#'>Delete</a>"
+                output += "<a href='/restaurants/%s/delete'>Delete</a>" % restaurant.id
                 output += "</br></br></br>"
-
             output += "</body></html>"
             self.wfile.write(bytes(output, 'utf8'))
+            return
+
+        if self.path.endswith("/edit"):
+            restaurantIDPath = self.path.split("/")[2]
+            myRestaurantQuery = session.query(Restaurant).filter_by(
+                id=restaurantIDPath).one()
+            if myRestaurantQuery:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = "<html><body>"
+                output += "<h1>"
+                output += myRestaurantQuery.name
+                output += "</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                output += "<input type = 'submit' value = 'Rename'>"
+                output += "</form>"
+                output += "</body></html>"
+
+                self.wfile.write(bytes(output, 'utf8'))
+                return
+        if self.path.endswith("/edit"):
+            restaurantIDPath = self.path.split("/")[2]
+            myRestaurantQuery = session.query(Restaurant).filter_by(
+                id=restaurantIDPath).one()
+            if myRestaurantQuery:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = "<html><body>"
+                output += "<h1>"
+                output += myRestaurantQuery.name
+                output += "</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                output += "<input type = 'submit' value = 'Rename'>"
+                output += "</form>"
+                output += "</body></html>"
+
+                self.wfile.write(bytes(output, 'utf8'))
+                return
+        if self.path.endswith("/delete"):
+            restaurantIDPath = self.path.split("/")[2]
+
+            myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+            if myRestaurantQuery:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Are you sure you want to delete %s?" % myRestaurantQuery.name
+                output += "<form method='POST' enctype = 'multipart/form-data' action = '/restaurants/%s/delete'>" % restaurantIDPath
+                output += "<input type = 'submit' value = 'Delete'>"
+                output += "</form>"
+                output += "</body></html>"
+                self.wfile.write(bytes(output, 'utf8'))
             return
         if self.path.endswith("/restaurants/new"):
             output = ""
@@ -96,6 +155,39 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     self.send_header('Content-type', 'text/html')
                     self.send_header('Location', '/restaurants')
                     self.end_headers()
+                return
+
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    session.delete(myRestaurantQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+                return
+
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers['content-type'])
+                pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+                if ctype == "multipart/form-data":
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0].decode("utf-8")
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+                    return
         except:
             pass
 
